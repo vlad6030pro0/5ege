c412452702864bd95a22226c15830a1f32479c8a
N4K <pronin6030@gmail.com>
2023-03-12 14:27:21 +0300

    Initial commit

---
+++ 5ege/_5ege.py
@@ -0,0 +1,134 @@
+'''
+for i in range(1,1000):
+    b = int(bin(i))
+    k=str(b%2)+str(b//2%2)
+    a = int(bin(i)+k,2)
+    if(a>74):
+        print(i)
+        break
+'''
+'''
+def schet(i):
+    a = 0
+    a+=int(i[:1])
+    i=i[1:]
+    a+=int(i[:1])
+    return a
+
+for i in range(1000,10000):
+    p = i
+    a,b,c = 0,0,0
+    abc = [a,b,c]
+    for n in range(3):
+        abc[n]+=schet(str(i))
+        i=str(i)[1:]
+    abc.remove(min(abc))
+    l = str(abc[0])+str(abc[1])
+    if l=="1418":
+        print(p)
+        break
+'''
+'''
+def schet(a):
+    b = 0
+    while a:
+        b+=a%2
+        a//=2
+    return b
+for i in range(1,1000):
+    r = bin(i)[2:]+str(schet(int(bin(i)[2:]))%2)
+    r = str(r)+str(schet(int(r))%2)
+    if int(r,2)>154:
+        print(int(r,2))
+        print(i)
+        break
+    '''
+'''
+def schet(i):
+    a = 0
+    a += int(i[:1])
+    i = i[1:]
+    a += int(i[:1])
+    return a
+
+ans = []
+for i in range(1000,10000):
+    p = i
+    a = 0
+    b = 0
+    c = 0
+    abc = [a,b,c]
+    i = str(i)
+    for n in range(3):
+        abc[n] = schet(i)
+        i = str(i)[1:]
+    abc.remove(min(abc))
+    l = str(min(abc))+str(max(abc))
+    if l == "613":
+        ans.append(p)
+print(max(ans))
+'''
+'''
+for i in range(1,1000):
+
+    r = bin(i)[2:]
+    if int(r)%2==0:
+        r = '1'+r+'0'
+    else:
+        r = '11'+r+'11'
+    if (int(r,2)>52):
+        print(i)
+        break
+'''
+def proverka(i):
+    a = 0
+    while i:
+        if i%10%2==1:
+            a+=1
+        i//=10
+    if a==4:
+        return True
+    return False
+def s(i):
+    i = str(i)
+    b=0
+    c=0
+    a = ''
+    b+=int(i[:1])
+    i=i[1:]
+    b+=int(i[:1])
+    i=i[1:]
+    c+=int(i[:1])
+    i=i[1:]
+    c+=int(i[:1])
+    a += str(min(b,c))+str(max(b,c))
+    return a
+
+c=0
+for i in range(1000,10000):
+    if proverka(i):
+        if (s(i)=="616"):
+            c+=1
+print(c)
+
+        
+
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
